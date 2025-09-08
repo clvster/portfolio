d2c498b4bd0ae581453110252334bc28a81bdf83
Cluster <cassettecluster@gmail.com>
2025-09-09 03:03:15 +0700

    feat: home, mobile support update: header and assets

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://x.com/rlycluster" className="home__social-icon" target="_blank">
+                <i className="uil uil-twitter-alt"></i> </a>
+
+            <a href="https://t.me/clvster" className="home__social-icon" target="_blank">
+                <i className="uil uil-telegram-alt"></i>
+            </a>
+
+            <a href="https://github.com/clvster" className="home__social-icon" target="_blank">
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
